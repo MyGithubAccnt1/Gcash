8b8245b35a86413ce31ac64d0ae004431f023300
me.mvbernabe@gmail.com <me.mvbernabe@gmail.com>
2025-07-28 11:31:40 +0800

    add total result

--- src/App.jsx
+++ src/App.jsx
@@ -36,25 +36,31 @@ function App() {
   }, []);
   return (
     <>
-      <div className="h-screen flex flex-col bg-gradient-to-t from-violet-500 to-blue-500 text-white">
-        <div className="flex items-center justify-center mb-15!">
-          <img src="cashg.png" className="h-15" />
-          <b className="text-[35px]">Cash</b>
-        </div>
+      <div className="h-screen flex bg-gradient-to-t from-violet-500 to-blue-500 text-white relative">
+        {/* <div className="h-screen flex flex-col w-[320px] bg-[rgba(255,255,255,0.1)]">
+          
+        </div> */}
+        <div className="flex flex-col w-full">
+          <div className="flex items-center justify-center mb-15!">
+            <img src="cashg.png" className="h-15" />
+            <b className="text-[35px]">Cash</b>
+          </div>
 
-        <div className="flex flex-col items-center md:flex-row md:justify-center md:items-start gap-2 mb-5!">
-          <Search setSearch={setSearch} setFilter={setFilter} />
-          <AddButton data={data} setData={setData} />
-        </div>
+          <div className="flex flex-col items-center md:flex-row md:justify-center md:items-start gap-2 mb-5!">
+            <Search setSearch={setSearch} setFilter={setFilter} />
+            <AddButton data={data} setData={setData} />
+          </div>
 
-        <div className="flex-grow overflow-hidden">
-          <FetchData
-            data={data}
-            setData={setData}
-            search={search}
-            filter={filter}
-          />
+          <div className="flex-grow overflow-hidden">
+            <FetchData
+              data={data}
+              setData={setData}
+              search={search}
+              filter={filter}
+            />
+          </div>
         </div>
+        
       </div>
     </>
   );

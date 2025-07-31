382cae6c32ad14548ecf06215c6fb742c1d91bd3
me.mvbernabe@gmail.com <me.mvbernabe@gmail.com>
2025-07-31 16:17:30 +0800

    fix overflow

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ function App() {
   return (
     <>
       {loading && <Loader />}
-      <div className="h-screen flex flex-col bg-gradient-to-t from-violet-500 to-blue-500 text-white relative">
+      <div className="h-[100dvh] flex flex-col bg-gradient-to-t from-violet-500 to-blue-500 text-white">
 
         <div className="flex items-center justify-center">
           <img src="cashg.png" className="h-15" />
@@ -41,7 +41,7 @@ function App() {
 
         <Burger button={button} setButton={setButton}/>
 
-        <div className="flex relative">
+        <div className="flex flex-1 relative overflow-hidden">
 
           <div className={`h-screen flex flex-col items-center justify-center text-black font-bold w-[320px] bg-[rgba(255,255,255,0.3)] absolute md:relative md:left-0 top-0 transition-all duration-500 ${button ? 'left-0' : '-left-[100%]'}`}>
             DASHBOARD COMING SOON

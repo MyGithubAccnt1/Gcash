712d9274ab8516d9c7bbd44d4b4cff25311898a2
john <john.castillo@journeytech.com.ph>
2025-08-01 16:18:27 +0800

    feat: integrate Kanban board into main application and improve local storage handling

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import AddButton from "./component/addButton";
 import FetchData from "./component/fetchData";
 import { useEffect, useState } from "react";
 import Loader from "./component/loader";
-import { GS_DATA_URL } from"./utils/constant";
+import { GS_DATA_URL } from "./utils/constant";
 import Burger from "./component/burger";
+import CanbanBoard from "./views/canban/index";
+
 function App() {
   const [data, setData] = useState("");
   const [filter, setFilter] = useState("");
@@ -21,7 +23,7 @@ function App() {
         setData(Array.isArray(result) ? result : []);
         setLoading(false);
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
         setData([]);
       }
     };
@@ -33,22 +35,23 @@ function App() {
     <>
       {loading && <Loader />}
       <div className="h-[100dvh] flex flex-col bg-gradient-to-t from-violet-500 to-blue-500 text-white">
-
         <div className="flex items-center justify-center">
           <img src="cashg.png" className="h-15" />
           <b className="text-[35px]">Cash</b>
         </div>
 
-        <Burger button={button} setButton={setButton}/>
+        <Burger button={button} setButton={setButton} />
 
         <div className="flex flex-1 relative overflow-hidden">
-
-          <div className={`h-screen flex flex-col items-center justify-center text-black font-bold w-[320px] bg-[rgba(255,255,255,0.3)] absolute md:relative md:left-0 top-0 transition-all duration-500 ${button ? 'left-0' : '-left-[100%]'}`}>
+          <div
+            className={`h-screen flex flex-col items-center justify-center text-black font-bold w-[320px] bg-[rgba(255,255,255,0.3)] absolute md:relative md:left-0 top-0 transition-all duration-500 ${
+              button ? "left-0" : "-left-[100%]"
+            }`}
+          >
             DASHBOARD COMING SOON
           </div>
 
           <div className="flex flex-col w-full">
-
             <div className="flex flex-col items-center md:flex-row md:justify-center md:items-start gap-2 mb-5!">
               <Search setSearch={setSearch} setFilter={setFilter} />
               <AddButton data={data} setData={setData} />
@@ -62,12 +65,11 @@ function App() {
                 filter={filter}
               />
             </div>
-
           </div>
-
         </div>
-        
       </div>
+
+      <CanbanBoard />
     </>
   );
 }

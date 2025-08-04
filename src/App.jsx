dafa4229e80e279f7b144cd1bb1a9616209e75d4
me.mvbernabe@gmail.com <me.mvbernabe@gmail.com>
2025-08-04 11:59:25 +0800

    feat: update App component navigation and enhance FetchData component with table layout for better data presentation

--- src/App.jsx
+++ src/App.jsx
@@ -15,12 +15,21 @@ function App() {
 
         <div className="flex flex-1 relative overflow-hidden">
           <div
-            className={`h-screen flex flex-col items-center !py-5 gap-1 text-black font-bold w-[320px] bg-[rgba(255,255,255,0.1)] absolute md:relative md:left-0 top-0 transition-all duration-500 ${
+            className={`h-screen flex flex-col items-center gap-[2px] text-gray-500 font-bold w-[320px] bg-[rgba(255,255,255,0.1)] absolute md:relative md:left-0 top-0 transition-all duration-500 ${
               button ? "left-0" : "-left-[100%]"
             }`}
           >
-            <NavLink to='/' className='bg-[rgba(255,255,255,0.2)] w-full !ps-5 !py-3 transition-all duration-300 hover:bg-[rgba(255,255,255,0.3)]'>Home</NavLink>
-            <NavLink to='/kanban-board' className='bg-[rgba(255,255,255,0.2)] w-full !ps-5 !py-3 transition-all duration-300 hover:bg-[rgba(255,255,255,0.3)]'>Feature Discussion</NavLink>
+            <NavLink to='/' className='bg-[rgba(255,255,255,0.2)] w-full flex !ps-5 !py-3 transition-all duration-300 hover:bg-[rgba(255,255,255,0.3)]'>
+              <h1 className="bg-gradient-to-r from-white to-gray-300 bg-clip-text text-transparent">
+                Main Page
+              </h1>
+            </NavLink>
+
+            <NavLink to='/kanban-board' className='bg-[rgba(255,255,255,0.2)] w-full flex !ps-5 !py-3 transition-all duration-300 hover:bg-[rgba(255,255,255,0.3)]'>
+              <h1 className="bg-gradient-to-r from-white to-gray-300 bg-clip-text text-transparent">
+                Feature Discussion
+              </h1>
+            </NavLink>
           </div>
 
           <div className="flex flex-col w-full overflow-auto">
--- src/component/fetchData.jsx
+++ src/component/fetchData.jsx
@@ -40,46 +40,49 @@ export default function FetchData({ data, search, filter }) {
           </b>
         </div>
       </div>
-      {data.length > 0 ? (
-        data
-          .slice()
-          .reverse()
-          .map((item, index) =>
-            filter && item.mode !== filter ? null : item.mode
-                .toLowerCase()
-                .includes(search.toLowerCase()) ||
-              item.to.toLowerCase().includes(search.toLowerCase()) ||
-              item.amount.toString().includes(search) ||
-              item.refNo.toString().includes(search) ||
-              item.date.toString().includes(search) ? (
-              <div
-                key={index}
-                className="bg-[rgba(255,255,255,0.1)] rounded-md p-5!"
-              >
-                <b>
-                  Mode: {item.mode}
-                  <br />
-                  {item.to && (
-                    <>
-                      To: {item.to}
-                      <br />
-                    </>
-                  )}
-                  Amount: PHP {item.amount}
-                  <br />
-                  Ref No.: {item.refNo}
-                  <br />
-                  Date: {item.date}
-                  <br />
-                </b>
-              </div>
-            ) : null
-          )
-      ) : (
-        <div className="bg-[rgba(255,255,255,0.1)] rounded-md p-5!">
-          <i>No records were found.</i>
-        </div>
-      )}
+      <table className="w-full bg-[rgba(255,255,255,0.1)] rounded-md">
+        <thead className="border-b border-gray-300">
+          <tr>
+            <th className="w-[5%]"></th>
+            <th className="w-[10%] !py-3"><b>Mode</b></th>
+            <th className="w-[20%]"><b>To</b></th>
+            <th className="w-[15%]"><b>Amount</b></th>
+            <th className="w-[25%]"><b>Ref No.</b></th>
+            <th className="w-[25%]"><b>Date</b></th>
+          </tr>
+        </thead>
+        <tbody className="border-b border-gray-300">
+          {data.length > 0 ? (
+            data
+              .slice()
+              .reverse()
+              .map((item, index) =>
+                filter && item.mode !== filter ? null : item.mode
+                    .toLowerCase()
+                    .includes(search.toLowerCase()) ||
+                  item.to.toLowerCase().includes(search.toLowerCase()) ||
+                  item.amount.toString().includes(search) ||
+                  item.refNo.toString().includes(search) ||
+                  item.date.toString().includes(search) ? (
+                  <tr role="row" key={index} className={index % 2 === 0 ? "bg-[rgba(255,255,255,0.1)]" : ""}>
+                    <td className="!py-2 border border-y-0 border-gray-300 border-l-0 text-center text-md text-gray-300">{index + 1}</td>
+                    <td className="!px-2 border border-y-0 border-gray-300 text-sm">{item.mode}</td>
+                    <td className="!px-2 border border-y-0 border-gray-300 text-sm">{item.to}</td>
+                    <td className="!px-2 border border-y-0 border-gray-300 text-sm text-end">{item.amount}</td>
+                    <td className="!px-2 border border-y-0 border-gray-300 text-sm">{item.refNo}</td>
+                    <td className="!px-2 border border-y-0 border-gray-300 border-r-0 text-sm">{item.date}</td>
+                  </tr>
+                ) : null
+              )
+          ) : (
+            <>
+              <tr role="row">
+                <td colSpan='6'>No records were found.</td>
+              </tr>
+            </>
+          )}
+        </tbody>
+      </table>
     </div>
   );
 }
